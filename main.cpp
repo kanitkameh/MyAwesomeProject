3312c25c596475194fc41743ae24a65ec47b451a
Kamen Vakavchiev <kanitkameh@gmail.com>
2020-12-14 17:36:03 +0200

    implement biggest number

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+int biggestNumber(int* arr, int size){
+	int result = arr[0];
+	for(int i=0;i<size;i++){
+		if(result<arr[i]){
+			result = arr[i];
+		}
+	}
+	return result;
+}
+
 int main(){
     return 0;
 }
